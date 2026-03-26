0c2abece3b90ffc743fea858f679b36898b4f03e
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2026-03-27 02:36:19 +0530

    auth init

---
+++ apps/primary-backend/src/modules/auth/index.ts
@@ -0,0 +1,12 @@
+import { Elysia } from "elysia";
+
+
+export const auth = new Elysia({prefix: "/auth"})
+                    .post('/sign-up', async ({body}) => {
+                        const response = await 
+                    },{
+                        
+                    })
+                    .post('sign-in', async ({body}) => {
+                        
+                    })
